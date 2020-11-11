77f5c12281a474530ec31f804b03cfba71f24d4d
Ryan Singh <ryan2229@hotmail.com>
2020-11-11 20:15:59 +0000

    Building main scene and sandbox stuff for game

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float maxZLimit = 7.5f;
+    private float minZLimit = -7.5f;
+
+    [SerializeField] private float zMoveAmount = 5f;
+    
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("a"))
+        {
+            var zPos = transform.position.z + zMoveAmount * Time.deltaTime;
+            transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.Clamp(zPos,minZLimit, maxZLimit));
+        }
+        if (Input.GetKey("d"))
+        {
+            var zPos = transform.position.z + -zMoveAmount * Time.deltaTime;
+            transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.Clamp(zPos,minZLimit, maxZLimit));
+        }
+    }
+}
