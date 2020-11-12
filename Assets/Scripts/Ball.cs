6f261e6423566a4c9802c47161676ac37355e003
Ryan Singh <ryan2229@hotmail.com>
2020-11-12 13:05:53 +0000

    Added better physics materials for all elements and ball spawner script

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    #region ScriptableObjects
+
+    [SerializeField] private BallData _ballData;
+
+    #endregion
+
+    #region Vars
+
+    [SerializeField] private Rigidbody _rigidbody;
+
+    #endregion
+
+
+    public void ApplySpawnForce()
+    {
+        _rigidbody.AddForce(_ballData.GetInitialSpawnForce(transform), ForceMode.Impulse);
+    }
+}
---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+   [SerializeField] private Transform _spawnPoint;
+
+   [SerializeField] private Ball _ballPrefab;
+
+   public bool spawnBall;
+   public float power;
+
+   private void Update()
+   {
+      if (spawnBall)
+      {
+         spawnBall = false;
+         SpawnBall();
+      }
+   }
+   
+   private void SpawnBall()
+   {
+      Ball ball = Instantiate(_ballPrefab, _spawnPoint);
+      ball.ApplySpawnForce();
+   }
+   
+}
