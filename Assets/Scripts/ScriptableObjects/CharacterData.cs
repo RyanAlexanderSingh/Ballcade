f6fd53114787c283898f8118fb337136e92ce9dd
Ryan Singh <ryan2229@hotmail.com>
2020-11-16 16:29:43 +0000

    Adding new character materials

---
+++ Assets/Scripts/ScriptableObjects/CharacterData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharacterData", menuName = "Ballcade/Character Data")]
+public class CharacterData : ScriptableObject
+{
+    #region Vars
+
+    [SerializeField] private Material _modelMaterial;
+
+    #endregion
+
+    public Color GetCharacterColour()
+    {
+        return _modelMaterial.color;
+    }
+
+    public Material GetCharacterMaterial()
+    {
+        return _modelMaterial;
+    }
+}
